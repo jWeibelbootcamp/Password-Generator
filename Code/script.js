9aaa17bb22c9d0cfb6fb3b611a756d2f1a5c1a11
devWeibel <112207453+devWeibel@users.noreply.github.com>
2022-09-15 09:15:53 -0400

    finished user prompts except maybe the very first condition check

--- Code/script.js
+++ Code/script.js
@@ -4,28 +4,18 @@ var lowerCase;
 var numbers;
 var specialChars;
 
-var promptCheck;
+var clicked; //using this to check if button is pressed i guess
+var promptCheck; //makes sure an acceptable character count is entered
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
+// Add event listener to "Generate Password" button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-//
-if (document.getElementById("generate").clicked == true) { //this may be bullshit
+//Gather User Input
+if (document.getElementById("generate").clicked == true) { //this may be bullshit - supposed to be a condition that the "Generate Password" button has been clicked.
   charNumber = prompt("How many characters would you like?\n(Between 8 and 128)");
 }  
 
@@ -37,22 +27,25 @@ if (charNumber < 8 || typeof(charNumber) != "number") {
   } else {
     promptCheck = true;
 }
-//Now charNumber should have a useable value and promptCheck should have a value of true.  
+//Now charNumber should have an acceptable value and promptCheck should have a value of true.  
 
 // Prompt for remaining inputs.
 if (promptCheck == true) {
-  upperCase = prompt("Would you like to include Uppercase Characters?\n(OK for yes, Cancel for no)");
+  upperCase = prompt("Would you like to include Uppercase Characters?\n(OK = yes, Cancel = no)"); //i assume this assigns a boolean value to upperCase.
 }
 
-if (upperCase == ) //find out what is returned upon OK and Cancel button press.
+lowerCase = prompt("Would you like to include Lowercase Characters?\n(OK = yes, Cancel = no)");
 
-var lowerCase = prompt("Would you like to include Lowercase Characters?\n(OK for yes, Cancel for no)");
+numbers = prompt("Would you like to include Numbers?\n(OK = yes, Cancel = no)");
 
+specialChars = prompt("Would you like to include Special Characters?\n(OK = yes, Cancel = no)");
 
-var numbers = prompt("Would you like to include Numbers?\n(OK for yes, Cancel for no)");
+//Generate the password
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var specialChars = prompt("Would you like to include Special Characters?\n(OK for yes, Cancel for no)");
-
-
-
+  passwordText.value = password;
+}
